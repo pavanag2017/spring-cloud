b09a1ebfef4f59628ba1a48e92f21befdb6f9031
Pavan Agrawal <pavanag@gmail.com>
2019-05-21 08:38:53 +0530

    Add files via upload
    Services as Module

---
+++ ratingsdata/src/main/java/cts/cloud/example/demo/rating/Application.java
@@ -0,0 +1,13 @@
+package cts.cloud.example.demo.rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
